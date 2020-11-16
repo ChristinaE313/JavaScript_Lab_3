f0d946c66d1ad766776fb4ceaf4d2da6f2a1013c
Christina Evans <chirstina.evans@perficient.com>
2020-11-16 17:50:01 -0500

    Adding final submission changes

--- javalab3.scripts.js
+++ javalab3.scripts.js
@@ -54,23 +54,23 @@ addSubmission(submissions, "Christina", 100, "2020-11-11");
 // ○ Functionality: remove the object from the array at the specified index using the
 // splice method.
 
-    // function deleteSubmissionByIndex(array, index){  
-    //    array.splice(index) ;   
-    //     console.log(array); 
-    //    }
-    // deleteSubmissionByIndex(submissions, 4, 1); 
+    function deleteSubmissionByIndex(array, index){  
+        array.splice(index) ;   
+         console.log(array); 
+        }
+     deleteSubmissionByIndex(submissions, 4, 1); 
 
 //Declare a function named deleteSubmissionByName
 // ○ Parameter(s): array, name
 // ○ Functionality: remove the object from the array that has the provided name.
 // Incorporate the findIndex method and the splice method.
 
-//      function deleteSubmissionByName(array, name){
-//          let index = array.findIndex (exactName => exactName.name === name);
-//          array.splice(index, 1); 
-//          console.log(array); 
-//   }
-//      deleteSubmissionByName(submissions, "Christina"); 
+      function deleteSubmissionByName(array, name){
+          let index = array.findIndex (exactName => exactName.name === name);
+          array.splice(index, 1); 
+          console.log(array); 
+   }
+      deleteSubmissionByName(submissions, "Christina"); 
 
 // Declare a function named editSubmission
 // ○ Parameter(s): array, index, score
@@ -78,81 +78,82 @@ addSubmission(submissions, "Christina", 100, "2020-11-11");
 // conditional statements to set the value for the passed property to true if the
 // score is greater than or equal to 60 and false otherwise
 
-// function editSubmission(array, index, score){ 
-//    let findScore = array[index];
-//    findScore.score = score;  
+     function editSubmission(array, index, score){ 
+        let findScore = array[index];
+        findScore.score = score;  
     
-//     let passed = findScore.passed; 
-//      if (passed >= 60) {
-//          findScore.passed = true; 
-//     } else {
-//          findScore.passed = false; 
-//     } 
-//     console.log(array); 
-// }
-// editSubmission(submissions, 4, 40); 
+         let passed = findScore.passed; 
+      if (passed >= 60) {
+          findScore.passed = true; 
+         } else {
+        findScore.passed = false; 
+     } 
+        console.log(array); 
+     }
+     editSubmission(submissions, 4, 40); 
 
 // Declare a function named findSubmissionByName
 // ○ Parameter(s): array, name
 // ○ Functionality: return the object in the array that has the provided name. Use the
 // find method
 
-    // function findSubmissionByName(array, name){
-    //     let result = array.find(objectName => objectName.name === name); 
-    //     console.log(result); 
-    // }
-    // findSubmissionByName(submissions, "Jane"); 
+     function findSubmissionByName(array, name){
+         let result = array.find(objectName => objectName.name === name); 
+         console.log(result); 
+     }
+     findSubmissionByName(submissions, "Jane"); 
 
 // Declare a function named findLowestScore
 // ○ Parameter(s): array
 // ○ Functionality: return the object in the array that has the lowest score. Use the
 // forEach method to loop through the whole array.   
 
-    //  function findLowestScore(array){
-    //      let lowest = null; 
-    //      array.forEach(function(submission){ 
+      function findLowestScore(array){
+          let lowest = null; 
+          array.forEach(function(submission){ 
         
-    //      if (lowest === null || lowest.score > submission.score){
-    //          lowest = submission; 
-    //  }}); 
-    //     console.log(lowest); 
-    //  } 
-    //  findLowestScore(submissions); 
+          if (lowest === null || lowest.score > submission.score){
+              lowest = submission; 
+      }}); 
+         console.log(lowest); 
+      } 
+      findLowestScore(submissions); 
 
     // Declare a function named findAverageScore
     // ○ Parameter(s): array
     // ○ Functionality: return the average quiz score. Use a for...of loop
 
     function findAverageScore(array){
-      let average = 0;
-      for(let i = 0; i < array.length; i++){
+      let sum = 0;
+      for( let i = 0; i < array.length; i++){
           sum += array[i];
           return sum / array.length; 
-      ); 
-      console.log(average); 
+      }; 
+      console.log(sum); 
     }
-
+    findAverageScore(submissions); 
+// ^^I was not able to complete this one due to time^^ 
 
     // Declare a function named filterPassing
     // ○ Parameter(s): array
     // ○ Functionality: return a new array using the filter method. The filter method
     // should find objects in the array that have passing scores.
     
-    // function filterPassing(array){
-    //     let passingScore = array.filter(submission => submission.score >= 60);
-    //     console.log(passingScore);  
-    // }
-    // filterPassing(submissions); 
+     function filterPassing(array){
+         let passingScore = array.filter(submission => submission.score >= 60);
+         console.log(passingScore);  
+     }
+     filterPassing(submissions); 
 
     // Declare a function named filter90AndAbove
     // ○ Parameter(s): array
     // ○ Functionality: return a new array using the filter method. The filter method
     // should find objects in the array that have scores greater than or equal to 90.   
 
-    // function filter90AndAbove(array){
-    // let filterNumber = array.filter(function(submission){
-    //     return submission.score >= 90; 
-    // }); 
-    //     console.log(filterNumber);    
-    // }
-    // filter90AndAbove(submissions); 
+     function filter90AndAbove(array){
+         let filterNumber = array.filter(function(submission){
+         return submission.score >= 90; 
+     }); 
+         console.log(filterNumber);    
+     }
+     filter90AndAbove(submissions); 
